17a25d6adfb7f49e3c300e158fe7a6e7d7bfa07f
juliusstr <julius@struwing.dk>
2024-01-08 15:43:25 +0100

    game screen loaded

--- src/main/java/dtu/dk/MainFX.java
+++ src/main/java/dtu/dk/MainFX.java
@@ -1,11 +1,16 @@
 package dtu.dk;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class MainFX extends Application {
+    AnchorPane pane;
     public static void main(String[] args) {
         launch(args);
     }
@@ -13,8 +18,14 @@ public class MainFX extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Word War!");
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root, 600, 500);
+        FXMLLoader loadrer = new FXMLLoader(getClass().getClassLoader().getResource("gameScreen.fxml"));
+        try {
+            pane = (AnchorPane) loadrer.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Scene scene = new Scene(pane, 1280, 720);
+        scene.getStylesheets().add("nice.css");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
