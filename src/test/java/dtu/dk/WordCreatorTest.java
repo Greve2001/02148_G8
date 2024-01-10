43fac984d330bc5201808d7323d9b9f2e20d270c
Silja <91070829+yech199@users.noreply.github.com>
2024-01-10 13:20:23 +0100

    Fixed JUnit test Made sure the test doesn't crash when the assert is not true

--- src/test/java/dtu/dk/WordCreatorTest.java
+++ src/test/java/dtu/dk/WordCreatorTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static org.junit.Assert.assertTrue;
+
 public class WordCreatorTest {
     List<String> words;
 
@@ -16,10 +18,14 @@ public class WordCreatorTest {
     }
     @Test
     public void noWordsWithWhitespace() {
+        boolean noWordsWithWhitespace = true;
+
         for (String word : words) {
-            if (word.contains(" "))
-                assert false;
+            if (word.contains(" ")) {
+                noWordsWithWhitespace = false;
+                break;
+            }
         }
-        assert true;
+        assertTrue(noWordsWithWhitespace);
     }
 }
