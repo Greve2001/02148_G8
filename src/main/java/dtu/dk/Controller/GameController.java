ff758414b7e92cbf4678934c03cbdb0c48e15ace
Ulleren <ulrikbp@gmail.com>
2024-01-11 10:21:52 +0100

    Made the UpdateChecker - need to implement sending this

--- src/main/java/dtu/dk/Controller/GameController.java
+++ src/main/java/dtu/dk/Controller/GameController.java
@@ -4,19 +4,20 @@ import dtu.dk.Exceptions.NoGameSetupException;
 import dtu.dk.GameConfigs;
 import dtu.dk.Model.Peer;
 import dtu.dk.Model.Player;
+import dtu.dk.Protocol;
 import dtu.dk.View.MainFX;
 import javafx.application.Platform;
 import javafx.util.Pair;
 import org.jspace.ActualField;
 import org.jspace.FormalField;
 import org.jspace.SequentialSpace;
+import org.jspace.Space;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-import static dtu.dk.Protocol.PLAYER_DROPPED;
-import static dtu.dk.Protocol.UPDATE;
+import static dtu.dk.Protocol.*;
 import static dtu.dk.Utils.getLocalIPAddress;
 
 public class GameController {
@@ -114,6 +115,7 @@ public class GameController {
         allPeers = setupController.getPeers();
         activePeers = new ArrayList<>(List.copyOf(allPeers));
         new Thread(new DisconnectChecker(this)).start();
+        new Thread(new UpdateChecker(this)).start();
         ui.changeScene(GameConfigs.JAVA_FX_GAMESCREEN);
     }
 
@@ -206,20 +208,22 @@ public class GameController {
     }
 }
 
-class DisconnectChecker implements Runnable{
+class DisconnectChecker implements Runnable {
 
-    ArrayList<Pair<Peer, Player>>  activePeerList;
-    public DisconnectChecker(GameController gameController){
+    ArrayList<Pair<Peer, Player>> activePeerList;
+
+    public DisconnectChecker(GameController gameController) {
         activePeerList = gameController.getActivePeers();
     }
-    public void run(){
+
+    public void run() {
         int nextPeerIndex = 1;
-        while(activePeerList.size() > 1){
+        while (activePeerList.size() > 1) {
             try { // get a non existing string in the remotespace of the person next in the disconnect line
-                    activePeerList.get(nextPeerIndex).getKey().getSpace().get(new ActualField("nonexist"));
+                activePeerList.get(nextPeerIndex).getKey().getSpace().get(new ActualField("nonexist"));
             } catch (InterruptedException e) {
                 //Communicate to all others that the person has disconnected - start from index 2 to exclude disconnected person
-                for(int index = 2; index < activePeerList.size(); index++){
+                for (int index = 2; index < activePeerList.size(); index++) {
                     try {
                         //TODO - this is not picked up by the other peers yet
                         activePeerList.get(index).getKey().getSpace().put(UPDATE, PLAYER_DROPPED, activePeerList.get(nextPeerIndex).getKey().getID());
@@ -227,10 +231,69 @@ class DisconnectChecker implements Runnable{
                         System.out.println("Another disconnect -.-");
                     }
                 }
-                if(activePeerList.size() > 1) {
+                if (activePeerList.size() > 1) {
                     activePeerList.remove(nextPeerIndex);
                 }
                 System.out.println("Player disconnectet. Active peer list size = " + activePeerList.size());
+            }
+        }
+    }
+}
+
+class UpdateChecker implements Runnable {
+    GameController gameController;
+    List<Pair<Peer, Player>> activePLayerList;
+    Space localSpace;
+
+    public UpdateChecker(GameController gameController) {
+        this.gameController = gameController;
+        this.activePLayerList = gameController.getActivePeers();
+        this.localSpace = activePLayerList.get(0).getKey().getSpace();
+    }
+
+    public void run() {
+        while (activePLayerList.size() > 1) {
+            try {
+                Object[] updateTup = localSpace.get( // Player list
+                        new ActualField(UPDATE),
+                        new FormalField(Protocol.class), // URIs
+                        new FormalField(Integer.class) // Order
+                );
+                switch ((Protocol) updateTup[1]) {
+                    case UPDATE_LIFE:
+                        //get the id we need to check
+                        //get the persons life and update it
+                        for(int index = 1; index < activePLayerList.size(); index++){
+                            //Check if the ID is correct
+                            if(activePLayerList.get(index).getKey().getID() == (Integer)updateTup[2]){
+                                Object[] lifeTup = activePLayerList.get(index).getKey().getSpace().query(
+                                        new ActualField(LIFE), // TODO - make each peer have LIFE in their space
+                                        new FormalField(Integer.class));
+                                activePLayerList.get(index).getValue().setLives((Integer)lifeTup[1]);
+                                break;
+                                //TODO - COULD HAVE - make this only check the people we display
+                            }
+                        }
+                        break;
+                    case UPDATE_DEATH:
+                        for(int index = 1; index < activePLayerList.size(); index++){
+                            if(activePLayerList.get(index).getKey().getID() == (Integer)updateTup[2]){
+                                activePLayerList.remove(index);
+                                break;
+                            }
+                        }
+                        break;
+                    case UPDATE_SEND_WORD:
+                        Object[] extraWordTup = localSpace.get(
+                                new ActualField(EXTRA_WORD),
+                                new FormalField(String.class));
+                        //TODO actually send the word..
+                        break;
+                    default:
+                        System.out.println("UpdateChecker error - wrong update protocol - did nothing..");
+                }
+            } catch (InterruptedException e) {
+                System.out.println("UpdateChecker error - Cant get local space - something is wrong??");
             }
         }
     }
--- src/main/java/dtu/dk/Model/Player.java
+++ src/main/java/dtu/dk/Model/Player.java
@@ -4,4 +4,20 @@ public class Player {
     private String username;
     private int lives = 3;
     private int score = 0;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
 }
